17df78d2b0f8bcd9a3bb22c48ae4304193f878f6
polypmer <exorable.ludos@gmail.com>
2016-08-09 11:26:11 -0400

    add review intent

--- app/src/main/java/com/workingagenda/democracydroid/AboutActivity.java
+++ app/src/main/java/com/workingagenda/democracydroid/AboutActivity.java
@@ -1,6 +1,7 @@
 package com.workingagenda.democracydroid;
 
 import android.app.Activity;
+import android.content.ActivityNotFoundException;
 import android.content.Intent;
 import android.graphics.Color;
 import android.net.Uri;
@@ -10,6 +11,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 /**
@@ -18,10 +21,11 @@ import android.widget.TextView;
 public class AboutActivity extends AppCompatActivity {
 
     //Declaire some variables
-    public TextView Txt1;
+    public TextView Txt1; // why are these public? what?
     public TextView Txt2;
     public TextView Txt3;
     public TextView Txt4;
+    private Button btnRev;
     /**
      * The fragment argument representing the section number for this
      * fragment.
@@ -48,6 +52,27 @@ public class AboutActivity extends AppCompatActivity {
         Txt3.setText(R.string.about_instructions);
         Txt4 = (TextView) findViewById(R.id.about_4);
         Txt4.setText(R.string.about_info);
+
+        btnRev = (Button) findViewById(R.id.reviewButton);
+
+        btnRev.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Uri uri = Uri.parse("market://details?id=" + getBaseContext().getPackageName());
+                Intent goToMarket = new Intent(Intent.ACTION_VIEW, uri);
+                // To count with Play market backstack, After pressing back button,
+                // to taken back to our application, we need to add following flags to intent.
+                goToMarket.addFlags(Intent.FLAG_ACTIVITY_NO_HISTORY |
+                        Intent.FLAG_ACTIVITY_NEW_DOCUMENT |
+                        Intent.FLAG_ACTIVITY_MULTIPLE_TASK);
+                try {
+                    startActivity(goToMarket);
+                } catch (ActivityNotFoundException e) {
+                    startActivity(new Intent(Intent.ACTION_VIEW,
+                            Uri.parse("http://play.google.com/store/apps/details?id=" + getBaseContext().getPackageName())));
+                }
+            }
+        });
 
     }
     @Override
