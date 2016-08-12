2bcf99e44255a398464f19d15ea7c20d0c3f2cac
polypmer <exorable.ludos@gmail.com>
2016-08-11 23:23:25 -0400

    created mediaactivity

---
+++ app/src/main/java/com/workingagenda/democracydroid/MediaActivity.java
@@ -0,0 +1,15 @@
+package com.workingagenda.democracydroid;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by fen on 8/11/16.
+ */
+public class MediaActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
