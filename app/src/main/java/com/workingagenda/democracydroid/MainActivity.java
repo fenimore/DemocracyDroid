85b2c124c14e4bbf0a92970d28694317213071b3
polypmer <exorable.ludos@gmail.com>
2016-08-12 15:28:24 -0400

    fix tab count to protect against crashes

--- app/src/main/java/com/workingagenda/democracydroid/MainActivity.java
+++ app/src/main/java/com/workingagenda/democracydroid/MainActivity.java
@@ -898,7 +898,7 @@ public class MainActivity extends AppCompatActivity {
         @Override
         public int getCount() {
             // Show 3 total pages.
-            return 4;
+            return 3;
         }
 
         @Override
