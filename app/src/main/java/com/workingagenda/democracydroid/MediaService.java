0ef085778f55d3b0306a8300c5e665929740a660
polypmer <exorable.ludos@gmail.com>
2017-02-10 18:38:16 -0500

    Fix notifcation reloading mediaActivity

--- app/src/main/java/com/workingagenda/democracydroid/MediaService.java
+++ app/src/main/java/com/workingagenda/democracydroid/MediaService.java
@@ -1,9 +1,12 @@
 package com.workingagenda.democracydroid;
 
+import android.app.Notification;
+import android.app.PendingIntent;
 import android.app.Service;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Binder;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.IBinder;
 import android.support.annotation.Nullable;
@@ -33,6 +36,13 @@ import com.google.android.exoplayer2.util.Util;
 
 public class MediaService extends Service {
 
+    private SimpleExoPlayer player;
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        player.release();
+    }
+
     private final IBinder binder = new LocalBinder();
 
     @Nullable
@@ -43,7 +53,14 @@ public class MediaService extends Service {
 
     @Override
     public void onCreate() {
-
+        BandwidthMeter bandwidthMeter = new DefaultBandwidthMeter();
+        TrackSelection.Factory videoFactory = new AdaptiveVideoTrackSelection.Factory(bandwidthMeter);
+        TrackSelector trackSelector = new DefaultTrackSelector(videoFactory);
+        // Load controls
+        LoadControl loadControl = new DefaultLoadControl();
+        // Create Player
+        player = ExoPlayerFactory.newSimpleInstance(getApplicationContext(),
+                trackSelector, loadControl);
     }
 
     @Override
@@ -52,26 +69,48 @@ public class MediaService extends Service {
     }
 
     public SimpleExoPlayer setUpPlayer(Uri url) {
-        BandwidthMeter bandwidthMeter = new DefaultBandwidthMeter();
-        TrackSelection.Factory videoFactory = new AdaptiveVideoTrackSelection.Factory(bandwidthMeter);
-        TrackSelector trackSelector = new DefaultTrackSelector(videoFactory);
-        // Load controls
-        LoadControl loadControl = new DefaultLoadControl();
-        // Create Player
-        SimpleExoPlayer player = ExoPlayerFactory.newSimpleInstance(getApplicationContext(),
-                trackSelector, loadControl);
-
         DataSource.Factory dataSourceFactory = new DefaultDataSourceFactory(getApplicationContext(),
                 Util.getUserAgent(this, "DemocracyDroid"));
         // Produces Extractor instances for parsing the media data.
         ExtractorsFactory extractorsFactory = new DefaultExtractorsFactory();
-
+        // TODO:
+        // https://realm.io/news/360andev-effie-barak-switching-exoplayer-better-video-android/
+        // HlsChunkSource.HlsChunkHolder
+        // HlsMasterPlaylist
         MediaSource mediaSource = new ExtractorMediaSource(url,
                 dataSourceFactory, extractorsFactory, null, null);
 
         player.setPlayWhenReady(true);
         player.prepare(mediaSource);
+
+        		//notification
+		Intent notIntent = new Intent(this, MediaActivity.class);
+        // TODO: bundle
+		notIntent.addFlags(Intent.FLAG_ACTIVITY_REORDER_TO_FRONT);
+		PendingIntent pendInt = PendingIntent.getActivity(this, 0,
+				notIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+
+		Notification.Builder builder = new Notification.Builder(this);
+
+		builder.setContentIntent(pendInt)
+		.setSmallIcon(R.drawable.ic_mic_none_black_24dp)
+		.setTicker("Democracy Now!")
+		.setOngoing(true)
+		.setContentTitle("Democracy Now!")
+		.setContentText("The War and Peace Report");
+        Notification not = null;
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN) {
+            not = builder.build();
+        }
+        startForeground(1333, not);
         return player;
+    }
+
+    @Override
+    public boolean onUnbind(Intent intent) {
+        player.stop();
+        player.release();
+        return false;
     }
 
     // TODO: startPlaying
