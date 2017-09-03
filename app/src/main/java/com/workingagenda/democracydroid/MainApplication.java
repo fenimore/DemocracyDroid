4d54696acd13aac76a4a5b5582a7937a19577dd8
derrick rocha <derrickrocha@derricks-MacBook-Pro.local>
2017-09-03 14:42:56 -0600

    removed unused imports.

--- app/src/main/java/com/workingagenda/democracydroid/MainApplication.java
+++ app/src/main/java/com/workingagenda/democracydroid/MainApplication.java
@@ -6,15 +6,6 @@ import android.content.SharedPreferences;
 import android.preference.PreferenceManager;
 
 import com.facebook.drawee.backends.pipeline.Fresco;
-import com.workingagenda.democracydroid.Feedreader.RssReader;
-
-import org.jsoup.Jsoup;
-import org.jsoup.nodes.Document;
-
-import java.io.ByteArrayInputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.nio.charset.Charset;
 
 /**
  * Created by derrickrocha on 7/16/17.
